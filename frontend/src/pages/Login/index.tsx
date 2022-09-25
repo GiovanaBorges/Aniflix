2ec6805f3e7718437b8b8de266f35de6f8ea13dc
giovana borges <gigi12345as17@gmail.com>
2022-09-25 19:20:06 -0300

    :feat set color pattern

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -2,14 +2,27 @@
 function Login(){
     return(
         <>
-            <div className="container w-screen mx-auto columns-2 p-10">
-                <div className="">
-                    <h1>Login</h1>
-                </div>
-                <div className="">
-                    <h1>calguei</h1>
-                </div>
+                
+            <div className="text-white mx-auto">
+                <div className="grid grid-cols-2 ">
+               
                     
+                        <form className="flex flex-col bg-gray-900 h-full justify-center justify-items-center">
+                        <h1 className="mx-auto w-3/4 text-6xl font-light mb-3">Login</h1>
+                            <input className="focus:shadow-sm w-full focus:outline-none text-slate-400 placeholder-slate-400 rounded-md y-2 pl-10 p-3 w-3/4 mx-auto my-3 bg-zinc-900"  type="email" aria-label="Email" placeholder="Email"/>
+                            <input className="focus:shadow-sm w-full focus:outline-none text-slate-400 placeholder-slate-400 rounded-md y-2 pl-10 p-3 w-3/4 mx-auto my-3 bg-zinc-900" type="password" aria-label="Password" placeholder="Senha"/>
+                            
+                            <button className="m-4 p-3 w-3/4 border-solid border-2 border-zinc-800 mx-auto my-3 hover:border-stone-700 rounded-md text-3lg">Logar</button>
+
+                            <a className="mx-auto w-3/4 text-end mt-3 text-red-500 " href="/">Esqueci minha senha</a>  
+                            <a className="mx-auto w-3/4 mt-3 text-lg" href="/">Não tenho conta <span className="text-sky-500">Criar.</span> </a>  
+                        </form>
+                    
+                    <div className="w-full h-screen ">
+                        
+                        
+                    </div>
+                    </div>
             </div>
         </>
     )
