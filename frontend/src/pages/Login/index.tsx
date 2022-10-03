9f28ac5954fe9eb02b533ca378ce5e0ebfe0aae8
GiovanaBorges <gigi12345as17@gmail.com>
2022-10-03 06:00:46 -0300

    :style add responsibility to text

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -6,11 +6,11 @@ function Login(){
         <>
                 
             <div className="text-white mx-auto">
-                <div className="grid grid-cols-2 ">
+                <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1">
                
                     
                         <form className="flex flex-col bg-zinc-900 h-full justify-center justify-items-center">
-                        <h1 className="mx-auto w-3/4 text-6xl font-light mb-3">Olá novamente</h1>
+                        <h1 className="mx-auto w-3/4 sm:text-5xl lg:text-6xl md:text-6xl font-light mb-3">Olá novamente</h1>
                             {
                                 //<input className="focus:shadow-sm w-full focus:outline-none text-slate-400 placeholder-slate-400 rounded-md y-2 pl-10 p-3 w-3/4 mx-auto my-3 bg-zinc-900"  type="email" aria-label="Email" placeholder="Email"/>
                                 //<input className="focus:shadow-sm w-full focus:outline-none text-slate-400 placeholder-slate-400 rounded-md y-2 pl-10 p-3  my-3 bg-zinc-900" type="password" aria-label="Password" placeholder="Senha"/>
