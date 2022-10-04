8caae8bbb99fa623fb95bc31ff978bb131fae50e
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-10-04 11:58:33 -0300

    :fix change route name

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -7,7 +7,7 @@ function AniflixRoutes(){
         <>
             <Router>
                 <Routes>
-                    <Route element={<Login/>} path="/login"/>
+                    <Route element={<Login/>} path="/signin"/>
                     <Route element={<SignUp/>} path="/signup"/>
                 </Routes>
             </Router>
