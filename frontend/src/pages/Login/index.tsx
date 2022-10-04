73af50f32efb58e9ce7307b1080aeaaa2455c98a
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-10-04 11:59:03 -0300

    :style finish style screen

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -10,7 +10,7 @@ function Login(){
                
                     
                         <form className="flex flex-col bg-zinc-900 h-full justify-center justify-items-center">
-                        <h1 className="mx-auto w-3/4 sm:text-5xl lg:text-6xl md:text-6xl font-light mb-3">Olá novamente</h1>
+                        <h1 className="mx-auto w-3/4 sm:text-5xl lg:text-6xl md:text-6xl font-light mb-3">Hi again</h1>
                             {
                                 //<input className="focus:shadow-sm w-full focus:outline-none text-slate-400 placeholder-slate-400 rounded-md y-2 pl-10 p-3 w-3/4 mx-auto my-3 bg-zinc-900"  type="email" aria-label="Email" placeholder="Email"/>
                                 //<input className="focus:shadow-sm w-full focus:outline-none text-slate-400 placeholder-slate-400 rounded-md y-2 pl-10 p-3  my-3 bg-zinc-900" type="password" aria-label="Password" placeholder="Senha"/>
@@ -19,9 +19,9 @@ function Login(){
                                 <Input text="Email"/>
                                 <Input text="Password"/>
                             </div>
-                            <a className="mx-auto w-3/4 text-end my-4 text-lg text-red-500 hover:text-white " href="/">Esqueci minha senha</a>  
-                            <Button type="submit" text="Login"/>
-                            <a className="mx-auto w-3/4 mt-5 text-lg " href="/">Não tenho conta <span className="text-red-600 hover:text-white">Criar.</span> </a>  
+                            <a className="mx-auto w-3/4 text-end my-4 text-lg text-red-500 hover:text-white " href="/">I forgot my password</a>  
+                            <Button type="submit" text="Sign In"/>
+                            <a className="mx-auto w-3/4 mt-5 text-lg " href="/signup">I don't have an account <span className="text-red-600 hover:text-white">Sign Up.</span> </a>  
                         </form>
                     
                     
