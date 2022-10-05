7c25d7a7161786882df5037c9bebc4956b1bc08a
GiovanaBorges <gigi12345as17@gmail.com>
2022-10-05 00:57:47 -0300

    edited position cards

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -5,13 +5,13 @@ function Home(){
     return(
     <>
         <Navbar />
-        <div className="container px-4">
+        <div className="px-4 min-h-screen w-full">
 
-        
-        <h1 className="text-white text-2xl m-4 font-bold">Animes Populares</h1>
-        <div className="flex justify-start">
-          
-            <Card
+        <div className="">
+            <h1 className="text-white text-2xl m-4 font-bold">Animes Populares</h1>
+
+        <div className="flex justify-start ">
+        <Card
                 descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
                 Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
                 Phasellus ut tinciduada. Quisqu leo a tempor. 
@@ -31,6 +31,35 @@ function Home(){
                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
                 studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1} key="2"
             />
+        </div>
+        
+        </div>
+        <div>
+            <h1 className="text-white text-2xl m-4 font-bold">Animes Populares</h1>
+
+        <div className="flex justify-start ">
+        <Card
+                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
+                Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
+                Phasellus ut tinciduada. Quisqu leo a tempor. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+                studio="aaaa" titulo="sdasd" qtdEp={1} qtdTemporada={1}
+            />
+             <Card
+                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
+                Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
+                Phasellus ut tincidunt felis, ac feuia nostra, per inceptos himenaeos."
+                studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1}
+            />
+            
+            <Card
+                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
+                Quisque fermentum lacinia mollis. Vivque dapibus sodales leo a tempor. 
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+                studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1} key="2"
+            />
+        </div>
+        
         </div>
         </div>
      </>
