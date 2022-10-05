86969ecd35e08d000bebbfff392c0e00a6c0ca7d
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-10-05 11:14:06 -0300

    :style remove margin auto

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -9,7 +9,7 @@ export function Button(props:Props){
              <button  id="first_name" 
              className="bg-red-600 mt-2
               text-gray-900 text-lg rounded-lg  font-medium
-             focus:ring-blue-500 focus:border-blue-500 block w-3/6 mx-auto
+             focus:ring-blue-500 focus:border-blue-500 block w-3/6 
              p-2.5  dark:border-gray-600 dark:placeholder-gray-400 
              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 {props.text}
