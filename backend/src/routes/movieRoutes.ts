1415a317b8ad7d19440f41c9593a87f68042a066
giovana.borges <gigi12345as17@gmail.com>
2023-05-16 00:07:55 -0300

    :feat add all movie Routes of Movies

--- backend/src/routes/movieRoutes.ts
+++ backend/src/routes/movieRoutes.ts
@@ -1,15 +1,34 @@
-const express = require("express")
+import express from "express"
 const movieRoutes = express.Router()
 
-import {CreateMovieController} from "../controllers/createMovieController/CreateMovieController"
-import { GetAllMovieController } from "../controllers/GetAllMovieController/GetAllMovieController"
+import {CreateMovieController} from "../modules/Movie/CreateMovie/CreateMovieController"
+import { GetAllMovieController } from "../modules/Movie/GetAllMovie/GetAllMovieController"
+import {EditMovieController} from "../modules/Movie/EditMovie/EditMovieController"
+import { DeleteMovieController } from "../modules/Movie/DeleteMovie/DeleteMovieController"
+import { GetMovieByGenreController } from "../modules/Movie/GetMovieByGenre/GetMovieByGenreController"
+import { GetMovieByTitleController } from "../modules/Movie/GetMovieByTitle/GetMovieByTitleController"
 
 const getAllMovieController = new GetAllMovieController()
 const createMovieController = new CreateMovieController()
+const editMovieController = new EditMovieController()
+const deleteMovieController = new DeleteMovieController()
 
+// Searchs 
+const getMovieGenreController = new GetMovieByGenreController()
+const getMovieTitleControler  = new GetMovieByTitleController()
+
+//MOVIES
 // list all available movies
-movieRoutes .get("/list",getAllMovieController.GetAllMovie)
+movieRoutes.get("/list",getAllMovieController.getAllMovie)
 // create a new movie
-movieRoutes .post("/",createMovieController.CreateMovie)
+movieRoutes.post("/addMovie",createMovieController.createMovie)
+// edit movie
+movieRoutes.put("/editMovie", editMovieController.execute)
+// delete movie
+movieRoutes.delete("/deleteMovie", deleteMovieController.execute)
 
-export {movieRoutes }
+// Searchs 
+movieRoutes.get("/searchByTitle",getMovieTitleControler.execute)
+movieRoutes.get("/searchByGenre",getMovieGenreController.execute)
+
+export {movieRoutes}
