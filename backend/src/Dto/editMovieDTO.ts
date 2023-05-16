658553a94b437cc7654024d33b03b91159617e82
giovana.borges <gigi12345as17@gmail.com>
2023-05-15 23:59:07 -0300

    :feat add DTO for movie Edit

---
+++ backend/src/Dto/editMovieDTO.ts
@@ -0,0 +1,10 @@
+export interface editMovieDTO{
+    idMovie:number,
+    titleMovie?:string 
+    coverImageMovie?:string,
+    imageMovie?:string,
+    durationMinutesMovie?: number,
+    release_dateMovie?: string,
+    synopsisMovie?: string,
+    genreMovie?: string
+}
