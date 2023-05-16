ec1c789b55c846415727326eebe02faf3abe6216
giovana.borges <gigi12345as17@gmail.com>
2023-05-16 00:02:38 -0300

    :feat changed button

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -7,11 +7,7 @@ export function Button(props:Props){
     return (
         <>
              <button  id="first_name" 
-             className="bg-red-600 mt-2
-              text-gray-900 text-lg rounded-lg  font-medium
-             focus:ring-blue-500 focus:border-blue-500 block w-3/6 mx-auto
-             p-2.5  dark:border-gray-600 dark:placeholder-gray-400 
-             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+             className="bg-red-600 hover:bg-red-500 text-white font-bold text-lg py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded ">
                 {props.text}
              </button>
         </>
