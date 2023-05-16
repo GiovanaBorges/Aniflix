19fc33d991f262e6339d1b5c2e1b4ce8e5803b6b
giovana.borges <gigi12345as17@gmail.com>
2023-05-16 00:00:55 -0300

    :feat add dtos of movie

---
+++ frontend/src/Dtos/MovieDTO.ts
@@ -0,0 +1,10 @@
+export interface Movie{
+    studio:string,
+    title:string,
+    qtdEp:number,
+    qtdTemporada:number,
+    linkImage:string,
+    coverImage:string,
+    synopsis:string,
+    image:string
+}
