b75867091827844fdaf0a567bb6a93bbf5262b04
giovana.borges <gigi12345as17@gmail.com>
2023-05-16 00:04:40 -0300

    :fix changed layout

--- frontend/src/pages/random/index.tsx
+++ frontend/src/pages/random/index.tsx
@@ -3,31 +3,29 @@ import { Button } from "../../components/button";
 import { Navbar } from "../../components/navbar";
 import {Props} from "../../components/card"
 
-
-
-export default function Random(Props:Props){
+export default function Random(){
     
     return(
         <>
         <Navbar />
-                <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 p-5 text-white">              
-                        <form className="flex flex-col bg-zinc-900 h-3/5 justify-center justify-items-center p-5 h-4/5">
-                        <h1 className="mx-auto w-3/4 sm:text-5xl lg:text-6xl md:text-6xl font-light mb-3">{Props.titulo}</h1>
+                <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 text-white w-4/5 mx-auto pt-3">              
+                        <form className="flex flex-col bg-zinc-900 h-3/5 justify-center justify-items-center h-4/5">
+                        <h1 className="mx-auto w-3/4 sm:text-5xl lg:text-6xl md:text-6xl font-light mb-3">O cao da sua mae</h1>
                         <div className="flex justify-around w-3/4   mt-3 mb-3">
                             <h3 className="text-zinc-400 font-bold ">1 Temporada</h3>
                             <h3 className="text-zinc-400 font-bold"> 20 Episodios</h3>
                         </div>
-                        <p className="mx-auto w-3/4 mt-5 text-lg " >{Props.descricao}</p>  
-                            <h3 className="my-4 w-3/4 mx-auto text-zinc-400 font-bold ">{Props.studio}</h3>
+                        <p className="mx-auto w-3/4 mt-5 text-lg " >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit donec morbi vulputate molestie. Vitae interdum ac mi et congue cras tortor pellentesque pulvinar. Sem in eget hendrerit sed. Non amet, eget nunc, fermentum risus congue massa, in.</p>  
+                            <h3 className="my-4 w-3/4 mx-auto text-zinc-400 font-bold ">Studio : Cuuuu</h3>
                             
-                            <div className="mx-auto flex w-3/4">
+                            <div className="flex justify-center w-full  ">
                                 <Button text="Assistir" type="submit"/>
                                 <button  id="first_name" 
-                                className="flex justify-around bg-red-300 mt-2
-                                    text-black text-lg rounded-lg  font-medium
-                                    focus:ring-blue-500 focus:border-blue-500 block w-auto mx-auto
-                                    p-2.5  dark:border-gray-600 dark:placeholder-gray-400 
-                                    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                className="flex justify-around 
+                                bg-fuchsia-700 ml-5 hover:bg-fuchsia-600
+                                border-b-4 border-fuchsia-800 hover:border-fuchsia-500
+                                py-2 px-4 border-b-4 text-lg
+                                    focus:ring-blue-500 focus:border-blue-500 block w-auto rounded">
                                              <img 
                                                 src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/E74C3C/external-the-square-box-with-a-shuffle-loop-arrows-for-the-music-playback-music-regular-tal-revivo.png"
                                                 className="mx-3"/> Random
@@ -39,7 +37,7 @@ export default function Random(Props:Props){
                         </form>
                     
                     
-                            <img className="h-4/5 w-full" src="./image-cover-1.png" alt="" />
+                            <img className="h-4/5" src="./image-cover-1.png" alt="" />
                     
                     </div>
             
