2e4de019f62fa0c048872449a9564670c1a6b0d9
giovana.borges <gigi12345as17@gmail.com>
2023-05-16 00:05:20 -0300

    :feat consuming all movies list

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,63 +1,45 @@
 import {Navbar} from "../../components/navbar"
 import {Card} from "../../components/card"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Movie } from "../../Dtos/MovieDTO"
 
 function Home(){
+    const [moviesAvailable,SetmoviesAvailable]:any = useState([])
+
+    useEffect(() =>{
+        const getAllMoviesAvailable = async () =>{
+            axios.get("http://localhost:8085/movies/list").then(res =>{
+                console.log(res.data)
+                SetmoviesAvailable(res.data)
+            }).catch(error =>{
+                console.log(error)
+            })
+    
+           
+        }
+
+        getAllMoviesAvailable()
+    },[])
+
+
     return(
     <>
         <Navbar />
         <div className="px-4 min-h-screen w-full">
 
-        <div className="">
-            <h1 className="text-white text-2xl m-4 font-bold">Animes Populares</h1>
-
-        <div className="flex justify-start ">
-        <Card
-                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
-                Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
-                Phasellus ut tinciduada. Quisqu leo a tempor. 
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-                studio="aaaa" titulo="sdasd" qtdEp={1} qtdTemporada={1}
-            />
-             <Card
-                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
-                Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
-                Phasellus ut tincidunt felis, ac feuia nostra, per inceptos himenaeos."
-                studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1}
-            />
-            
-            <Card
-                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
-                Quisque fermentum lacinia mollis. Vivque dapibus sodales leo a tempor. 
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-                studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1} key="2"
-            />
-        </div>
-        
-        </div>
         <div>
-            <h1 className="text-white text-2xl m-4 font-bold">Animes Populares</h1>
+            <h1 className="text-white text-2xl m-4 font-bold">Series Populares</h1>
 
-        <div className="flex justify-start ">
-        <Card
-                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
-                Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
-                Phasellus ut tinciduada. Quisqu leo a tempor. 
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-                studio="aaaa" titulo="sdasd" qtdEp={1} qtdTemporada={1}
+        <div className="grid grid-cols-3 gap-4">
+       
+        {moviesAvailable.map((movie:Movie)=>(
+                <Card
+                descricao={movie.synopsis}
+                studio="bosta" titulo={movie.title} qtdEp={1} qtdTemporada={1} linkImage={movie.image}
+                coverImage={movie.coverImage}
             />
-             <Card
-                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
-                Quisque fermentum lacinia mollis. Vivamus ac consectetur nisl, eget facilisis odio. Donec vel ornare enim. 
-                Phasellus ut tincidunt felis, ac feuia nostra, per inceptos himenaeos."
-                studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1}
-            />
-            
-            <Card
-                descricao="Integer vestibulum massa non justo consectetur commodo. Morbi eleifend volutpat diam. 
-                Quisque fermentum lacinia mollis. Vivque dapibus sodales leo a tempor. 
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-                studio="aaaa" titulo="sdasd" qtdEp={3} qtdTemporada={1} key="2"
-            />
+            ))}
         </div>
         
         </div>
