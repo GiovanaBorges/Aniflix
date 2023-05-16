2073d9c7c7e019aa6cea777a243a45ee7ebc8095
giovana.borges <gigi12345as17@gmail.com>
2023-05-16 00:07:22 -0300

    :feat add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 require("dotenv").config()
+import cors from 'cors';
 import express from "express"
 const app = express()
 import {routes} from "./routes"
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000,()=>{
+app.listen(8085,()=>{
     console.log("server listening")
 })
