763c26942c864c0140d6f5550b4ffa4ba82b1232
giovana.borges <gigi12345as17@gmail.com>
2023-06-05 20:40:28 -0300

    :feat add all serie DTOS needs

---
+++ backend/src/Dto/SerieDTOS/CreateSerieDTO.ts
@@ -0,0 +1,10 @@
+export interface CreateSerieDTO{
+    title:string 
+    coverImage:string,
+    image:string,
+    EpQuantity: number,
+    TempQuantity:number,
+    release_date: string,
+    synopsis: string,
+    genre: string
+}
---
+++ backend/src/Dto/SerieDTOS/editSerieDTO.ts
@@ -0,0 +1,11 @@
+export interface editSerieDTO{
+    idSerie:number,
+    titleSerie?:string 
+    coverImageSerie?:string,
+    imageSerie?:string,
+    EpQuantitySerie?: number,
+    TempQuantitySerie?:number,
+    release_dateSerie?: string,
+    synopsisSerie?: string,
+    genreSerie?: string
+}
