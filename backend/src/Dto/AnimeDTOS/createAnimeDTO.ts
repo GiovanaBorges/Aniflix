cdd5f947252020e54bdda5b16058bf005e96efe7
giovana.borges <gigi12345as17@gmail.com>
2023-06-05 20:40:50 -0300

    :feat add all anime  DTOS need

---
+++ backend/src/Dto/AnimeDTOS/createAnimeDTO.ts
@@ -0,0 +1,11 @@
+export interface createAnimeDTO{
+    title: string
+    coverImage: string
+    image:string
+    release_date:string
+    studio:string
+    synopsis:string
+    genre:string
+    EpQuantity:number
+    TempQuantity:number
+}
---
+++ backend/src/Dto/AnimeDTOS/editAnimeDTO.ts
@@ -0,0 +1,12 @@
+export interface editAnimeDTO{
+    idAnime :number
+    title?: string
+    coverImage?: string
+    image?:string
+    release_date?:string
+    studio?:string
+    synopsis?:string
+    genre?:string
+    EpQuantity?:number
+    TempQuantity?:number
+}
