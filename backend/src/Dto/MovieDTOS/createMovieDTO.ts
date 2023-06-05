daef2acebfd1cdab7a0eb3f7b5fd660519951c69
giovana.borges <gigi12345as17@gmail.com>
2023-06-05 20:40:00 -0300

    :feat added all DTOS useful

---
+++ backend/src/Dto/MovieDTOS/createMovieDTO.ts
@@ -0,0 +1,10 @@
+export interface createMovieDTO{
+    title:string 
+    coverImage:string,
+    image:string,
+    durationMinutes: number,
+    release_date: string,
+    studio :string,
+    synopsis: string,
+    genreMovie: string
+}
