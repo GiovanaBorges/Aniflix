c0a4b49f304d4643c53e52941378e1c2c7215f48
giovana.borges <gigi12345as17@gmail.com>
2023-06-05 21:22:34 -0300

    :feat added route for anime

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { movieRoutes } from "./movieRoutes";
+import { serieRoutes } from "./serieRoutes";
+import { animeRoutes } from "./animeRoutes";
 
 const routes = Router();               
 
 //route movie
-routes.use("/movies", movieRoutes);
+routes.use("/movies", movieRoutes)
+routes.use("/series", serieRoutes)
+routes.use("/anime", animeRoutes)
 
 export { routes };
