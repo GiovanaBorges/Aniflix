1349929ed99a9877c85c8070e2bd53468f2c49e0
giovana.borges <gigi12345as17@gmail.com>
2023-06-17 01:15:57 -0300

    :feat user case with hash pass added

--- backend/src/modules/User/CreateUser/CreateUserUseCase.ts
+++ backend/src/modules/User/CreateUser/CreateUserUseCase.ts
@@ -2,11 +2,16 @@ import { User } from "@prisma/client";
 import { CreateUserDTO } from "../../../Dto/UserDTOS/CreateUserDTO";
 import { prisma } from "../../../prisma/client";
 import { CriptFunction } from "../../../Utils/criptFunction";
+import { JwtLogin } from "../../../middleare/jwtLogin";
+import { NextFunction, Request,Response } from "express";
 
 export class CreateUserUseCase{
-    async handle(user:CreateUserDTO):Promise<User>{
+    async handle(user:CreateUserDTO ,req:Request ,res:Response,next:NextFunction):Promise<User>{
         const criptFunction = new CriptFunction()
         const passHash = criptFunction.CriptPass(user.password)
+        const jwtLogin = new JwtLogin
+     
+       
 
         const userData = await prisma.user.create({
             data : {
@@ -16,10 +21,13 @@ export class CreateUserUseCase{
             }
         })
 
+        jwtLogin.createJwtToken(user.email, req, res,next)
 
         if(!userData){
             throw new Error("Erro ao criar Conta")
         }
+
+        
 
         return userData
     }
