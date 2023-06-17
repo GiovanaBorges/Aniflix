0a0ee13ec50a3978f48f13019a3703c94cd54d96
giovana.borges <gigi12345as17@gmail.com>
2023-06-17 01:15:30 -0300

    :feat route create serie  with jwt verify

--- backend/src/routes/serieRoutes.ts
+++ backend/src/routes/serieRoutes.ts
@@ -1,10 +1,11 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 import { CreateSerieController } from "../modules/Series/CreateSerie/CreateSerieController"
 import { GetAllSerieController } from "../modules/Series/GetAllSerie/GetAllSerieController"
 import { GetSerieByTitleController } from "../modules/Series/GerSerieByTitle/GetSerieByTitleController"
 import { GetAllSerieByGenreController } from "../modules/Series/GetAllSerieByGenre/GetAllSerieByGenreController"
 import { DeleteSerieController } from "../modules/Series/DeleteSerie/DeleteSerieController"
 import { EditSerieController } from "../modules/Series/EditSerie/EditSerieController"
+import { JwtLogin } from "../middleare/jwtLogin"
 const serieRoutes = express.Router()
 
 const createSerieController = new CreateSerieController()
@@ -15,8 +16,20 @@ const editSerieController = new EditSerieController()
 const getSerieByTitleController = new GetSerieByTitleController()
 const getAllSerieByGenreController = new GetAllSerieByGenreController()
 
+//middleare 
+const jwtLogin = new JwtLogin()
+
+interface AuthenticateRequest extends Request {
+    user?: any;
+  }
+
+// Middleware to authenticate the token
+const authenticateToken = (req: AuthenticateRequest, res: Response, next: NextFunction) => {
+    jwtLogin.AuthenticateToken(req, res, next);
+  };
+
 //create Serie
-serieRoutes.post("/addSerie", createSerieController.execute)
+serieRoutes.post("/addSerie", authenticateToken,createSerieController.execute)
 
 //get all Series
 serieRoutes.get("/list", getAllSerieController.execute)
